cda8db3f506cd06ed2a930f5f2e3ca2460323f1d
Shubhang Dutta <143163978+shubhangdutta96@users.noreply.github.com>
2024-06-27 15:03:18 +0530

    Setup Slick database tables
    Defined TaskTable and UserTable using Slick, with mapping to tasks and users tables.

--- src/main/scala/Tables/Tables.scala
+++ src/main/scala/Tables/Tables.scala
@@ -11,3 +11,23 @@ case class User(id: Long, name: String, email: String)
 object User {
   val tupled: ((Long, String, String)) => User = (User.apply _).tupled
 }
+
+upled: ((Long, String, String)) => User = (User.apply _).tupled
+}
+
+class TaskTable(tag: Tag) extends Table[Task](tag, "tasks") {
+  def id: Rep[Long] = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def title: Rep[String] = column[String]("title")
+  def description: Rep[String] = column[String]("description")
+  def userId: Rep[Long] = column[Long]("user_id")
+
+  def * : ProvenShape[Task] = (id, title, description, userId) <> (Task.tupled, Task.unapply)
+}
+
+class UserTable(tag: Tag) extends Table[User](tag, "users") {
+  def id: Rep[Long] = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def name: Rep[String] = column[String]("name")
+  def email: Rep[String] = column[String]("email")
+
+  def * : ProvenShape[User] = (id, name, email) <> (User.tupled, User.unapply)
+}
