c974da18e69a124332e3e9d5a2c0a936eb0b1177
Shubhang Dutta <143163978+shubhangdutta96@users.noreply.github.com>
2024-06-27 15:11:19 +0530

    Add task creation route
    Implemented the route to create a new task in TaskRoutes.

---
+++ src/main/scala/TaskRoutes/TaskRoutes.scala
@@ -0,0 +1,32 @@
+import Tables.{Task, User}
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import com.typesafe.scalalogging.LazyLogging
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+import spray.json.DefaultJsonProtocol.*
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import akka.http.scaladsl.model.StatusCodes
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+
+class TaskRoutes(databaseService: DatabaseService)(implicit ec: ExecutionContext) extends JsonSupport {
+
+  val route: Route =
+    pathPrefix("tasks") {
+      pathEndOrSingleSlash {
+        post {
+          entity(as[Task]) { task =>
+            onSuccess(databaseService.createTask(task)) { createdTask =>
+              complete(createdTask)
+            }
+          }
+        }
+      }
+    }
+}
