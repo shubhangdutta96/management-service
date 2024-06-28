eb59af76547ce64718660880ba240fb2412f411a
Shubhang Dutta <143163978+shubhangdutta96@users.noreply.github.com>
2024-06-28 14:31:42 +0530

    Update JsonSupport.scala
    Update the method (apply) is inserted.

--- src/main/scala/JsonSupport/JsonSupport.scala
+++ src/main/scala/JsonSupport/JsonSupport.scala
@@ -3,6 +3,6 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val taskFormat: RootJsonFormat[Task] = jsonFormat4(Task)
-  implicit val userFormat: RootJsonFormat[User] = jsonFormat3(User)
+  implicit val taskFormat: RootJsonFormat[Task] = jsonFormat4(Task.apply)
+  implicit val userFormat: RootJsonFormat[User] = jsonFormat3(User.apply)
 }
