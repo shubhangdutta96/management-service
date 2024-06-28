0819aebf665ec6d5b6f5cfcbc1e01cb97cff41df
Shubhang Dutta <143163978+shubhangdutta96@users.noreply.github.com>
2024-06-28 14:42:00 +0530

    Update TaskRoutes.scala
    Remove unnecessary imports from TaskRoutes.scala and add up the welcome message.

--- src/main/scala/TaskRoutes/TaskRoutes.scala
+++ src/main/scala/TaskRoutes/TaskRoutes.scala
@@ -1,23 +1,19 @@
 import Tables.{Task, User}
-import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
-import com.typesafe.scalalogging.LazyLogging
-import akka.http.scaladsl.server.Directives.*
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
-import spray.json.DefaultJsonProtocol.*
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
 import scala.concurrent.ExecutionContext
-import scala.util.{Failure, Success}
+
 
 class TaskRoutes(databaseService: DatabaseService)(implicit ec: ExecutionContext) extends JsonSupport {
 
   val route: Route =
+  pathSingleSlash {
+    get {
+      complete("Welcome to the Task Management Service API")
+    }
+  } ~
     pathPrefix("tasks") {
       pathEndOrSingleSlash {
         post {
