1160454431fdb063b5efd8bf4afd17a416ff4b91
Shubhang Dutta <143163978+shubhangdutta96@users.noreply.github.com>
2024-06-28 14:29:33 +0530

    Update Tables.scala
    Update companion objects of Task and User case classes

--- src/main/scala/Tables/Tables.scala
+++ src/main/scala/Tables/Tables.scala
@@ -4,15 +4,15 @@ import slick.jdbc.PostgresProfile.api._
 import slick.lifted.{ProvenShape, Tag}
 
 case class Task(id: Long, title: String, description: String, userId: Long)
-object Task {
-  val tupled: ((Long, String, String, Long)) => Task = (Task.apply _).tupled
-}
+
 case class User(id: Long, name: String, email: String)
-object User {
-  val tupled: ((Long, String, String)) => User = (User.apply _).tupled
+
+object Task {
+  def tupled: ((Long, String, String, Long)) => Task = (Task.apply _).tupled
 }
 
-upled: ((Long, String, String)) => User = (User.apply _).tupled
+object User {
+  def tupled: ((Long, String, String)) => User = (User.apply _).tupled
 }
 
 class TaskTable(tag: Tag) extends Table[Task](tag, "tasks") {
